986d3388b3f734ee84e106fe8e1b546fef3d25bc
callum-snowflaketrial <147353394+callum-snowflaketrial@users.noreply.github.com>
2023-10-09 11:15:56 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
-import streamlit as st
+import streamlit
 
-st.title("My parents healthy new diner")
+streamlit.title("My parents healthy new diner")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
